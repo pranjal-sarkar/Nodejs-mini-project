08ac908f3c7751f5c175789bd47d9a27d7ba2969
Pranjal <tataisarkar2001@gmail.com>
2022-02-14 16:14:07 +0530

    Added procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 
 dataFromFile = fs.readFileSync('data.txt', 'utf-8');
 
